1e970db9bad6c6c23d61052c2fa22b359cea79e2
Shelley Davis-Wise <shelleydavis-wise@gmail.com>
2015-11-25 15:56:03 -0600

    pushing final copy

--- www/js/controllers/childDetailCtrl.js
+++ www/js/controllers/childDetailCtrl.js
@@ -3,49 +3,75 @@ app.controller('childDetailCtrl', ["$scope", "$firebaseArray", "$firebaseObject"
 
         $scope.childId = $stateParams.id;
 
-
 // Getting Firebase reference to perform incident calculations
-        var incidentRef = new Firebase("https://allowance-tracker.firebaseio.com/incidents")
-            .orderByChild("childId")
-            .equalTo($scope.childId);
-        $scope.incidents = $firebaseObject(incidentRef);
-
 // Getting Firebase reference to perform transaction calculations
-        var transactionRef = new Firebase("https://allowance-tracker.firebaseio.com/transactions")
-            .orderByChild("childId")
-            .equalTo($scope.childId);
-        $scope.transactions = $firebaseObject(transactionRef);
-        console.log($scope.transactions);
-
 // Getting Firebase reference to get access to the child object
         var childRef = new Firebase("http://allowance-tracker.firebaseio.com/children/" + $scope.childId)
         $scope.child = $firebaseObject(childRef);
          var transTotal = $scope.transactionTotal;
          var infractTotal = $scope.infractionTotal;
-        // $scope.calculations = function() {
-            $scope.incidents.$loaded(function() {
-                $scope.infractionTotal = 0
-                angular.forEach($scope.incidents, function(incident) {
-                    if (incident.childId === $scope.childId) {
-                        $scope.infractionTotal += parseFloat(incident.infractionValue);
-                    }
-                });
-                 console.log($scope.infractionTotal);
-                return $scope.infractionTotal;
-            });
-            $scope.transactions.$loaded(function() {
-                $scope.transactionTotal = 0
-                angular.forEach($scope.transactions, function(transaction) {
-                    if (transaction.id === $scope.childid) {
-                        $scope.transactionTotal += parseFloat(transaction.transactionValue);
-                    }
-                });
-                console.log($scope.transactionTotal)
-                return $scope.transactionTotal;
-            }) //me
 
-            $scope.child.$loaded(function() {
-                $scope.startDate = $scope.child.date_created;
+
+         var getInfractions = function(child) {
+          var incidentRef = new Firebase("https://allowance-tracker.firebaseio.com/incidents")
+            .orderByChild("childId")
+            .equalTo($scope.childId);
+
+            $scope.infractionTotal = 0;
+            incidentRef.once("value", function(action) {
+              $scope.incidents = action.val();
+              for (var key in $scope.incidents) {
+                $scope.infractionTotal += parseFloat($scope.incidents[key].infractionValue);
+              }
+              console.log("$scope.infractionTotal", $scope.infractionTotal);
+            })
+
+        // $scope.incidents = $firebaseObject(incidentRef);
+        //   console.log("I ran")
+
+        //       $scope.infractionTotal = 0;
+        //       angular.forEach($scope.incidents, function(incident) {
+        //           if (incident.childId === $scope.childId) {
+        //               $scope.infractionTotal += parseFloat(incident.infractionValue);
+        //           }
+        //       });
+        //        console.log($scope.infractionTotal);
+        //       return $scope.infractionTotal;
+          };
+
+
+          var getTransactions = function(child){
+            var transactionRef = new Firebase("https://allowance-tracker.firebaseio.com/transactions")
+                  .orderByChild("childId")
+                  .equalTo($scope.childId);
+
+            $scope.transactionTotal = 0;
+            transactionRef.once("value", function(trans) {
+              $scope.transactions = trans.val();
+              for (var key in $scope.transactions) {
+                $scope.transactionTotal += parseFloat($scope.transactions[key].transactionValue);
+              }
+              console.log("$scope.transactionTotal", $scope.transactionTotal);
+            })
+
+              // $scope.transactions = $firebaseObject(transactionRef);
+              // console.log($scope.transactions);
+
+              // $scope.transactionTotal = 0
+              // console.log("$scope.transactions", $scope.transactions);
+              // angular.forEach($scope.transactions, function(transaction) {
+              //   console.log(transaction);
+              //     if (transaction.id === $scope.childid) {
+              //         $scope.transactionTotal += parseFloat(transaction.transactionValue);
+              //     }
+              // });
+              // console.log($scope.transactionTotal)
+              // return $scope.transactionTotal;
+          };
+
+            $scope.child.$loaded(function(child) {
+                console.log("child", child);
+                $scope.startDate = child.date_created;
                 $scope.today = Date.now();
                 $scope.calcTime = $scope.today - $scope.startDate;
 
@@ -57,8 +83,9 @@ app.controller('childDetailCtrl', ["$scope", "$firebaseArray", "$firebaseObject"
                     $scope.addAllowance = $scope.allowance * $scope.days;
                     console.log("addAllowance",$scope.addAllowance)
                   };
-                  console.log($scope.transactionTotal);
-                  console.log($scope.infractionTotal);
+
+                  getTransactions(child);
+                  getInfractions(child);
 
                 $scope.runningTotal = $scope.transactionTotal + $scope.infractionTotal;
                 console.log($scope.runningTotal);
@@ -66,13 +93,12 @@ app.controller('childDetailCtrl', ["$scope", "$firebaseArray", "$firebaseObject"
                 $scope.availableBalance = 0;
                   if ($scope.child.id === $scope.childId) {
                       $scope.availableBalance = ($scope.workingBalance - $scope.runningTotal) + $scope.addAllowance;
-
                   } else {
                       $scope.availableBalance = ($scope.workingBalance - $scope.runningTotal) + $scope.addAllowance
                   };
                 console.log("Available Balance", $scope.availableBalance);
                     return $scope.availableBalance;
-            }); // the end
+            });
         }
     // }
 ]);
